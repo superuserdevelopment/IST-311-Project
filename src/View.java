7f9e6d1011accfcae9d7172c825f062e4ea09429
Jangrock <113255122+Jangrock@users.noreply.github.com>
2022-09-10 17:07:31 -0400

    Added attributes but stuck calling commodity

--- src/View.java
+++ src/View.java
@@ -1,4 +1,15 @@
 
 public class View {
+  private String apiKey;
+  private String apiUrl;
+  
+  public RESTServiceHandler(String apiKey, String apiUrl)
+  {
+    this.apiKey = apiKey;
+    this.apiUrl = apiUrl;
+  }
+  
+  public getCommodityData
+  
 
 }
