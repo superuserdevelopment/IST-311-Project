b3dadb9a891aba391e24750d7c68f02c73beeb52
Jangrock <113255122+Jangrock@users.noreply.github.com>
2022-09-10 17:22:30 -0400

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,15 +1,3 @@
 
 public class View {
-  private String apiKey;
-  private String apiUrl;
-  
-  public RESTServiceHandler(String apiKey, String apiUrl)
-  {
-    this.apiKey = apiKey;
-    this.apiUrl = apiUrl;
-  }
-  
-  public getCommodityData
-  
-
 }
