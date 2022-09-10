5297f2b125252829296d2911310893bc478094f6
Jangrock <113255122+Jangrock@users.noreply.github.com>
2022-09-10 17:23:11 -0400

    Commodity data left

--- src/RESTServiceHandler.java
+++ src/RESTServiceHandler.java
@@ -1,4 +1,17 @@
 
 public class RESTServiceHandler {
+  private String apiKey;
+  private String apiUrl;
+  
+  public RESTServiceHandler(String apiKey, String apiUrl)
+  {
+    this.apiKey = apiKey;
+    this.apiUrl = apiUrl;
+  }
+  
+  public getCommodityData
+  
+
+}
 
 }
