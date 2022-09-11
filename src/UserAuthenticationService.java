3248af41ef2597658498165f8368be834ccf6fc7
Arya Samarth <47243744+superuserdevelopment@users.noreply.github.com>
2022-09-10 23:03:18 -0400

    Update UserAuthenticationService.java

--- src/UserAuthenticationService.java
+++ src/UserAuthenticationService.java
@@ -4,15 +4,15 @@ public class UserAuthenticationService
 {
   ArrayList<String> users = new ArrayList<String>();
   
-  public object loginUser(String username, String password) {
+  public Object loginUser(String username, String password) {
     
   }
   
-  public object createNewUser(String name, String username, String password) {
+  public Object createNewUser(String name, String username, String password) {
     
   }
   
-  public object deleteUser(String username, String password) {
+  public Object deleteUser(String username, String password) {
     
   }
 
