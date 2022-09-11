107759641b96becc7440344c224ea737aff12830
Anupooja <113248548+Anupooja@users.noreply.github.com>
2022-09-10 21:26:04 -0400

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,57 @@
+import java.util.ArrayList;
 
 public class User {
-
+  
+  private String name;
+  private String username;
+  private String password;
+  ArrayList<String> holdings = new ArrayList<String>();
+  ArrayList<String> watchlist = new ArrayList<String>();
+  ArrayList<String> favorites = new ArrayList<String>();
+  
+ public User(String name, String username, String password) {
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+  
+ public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+  
+  public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+  
+  public void validatePassword(String password, String[] args) {
+    boolean isPasswordValid = true;
+    boolean isPasswordNotValid = false;
+  }
+  
+  public void setPassword(String username, String password, String[] args) {
+		this.username = username;
+    this.password = passwword;
+	}
+  
+  public void getHoldings(String[] args) {
+    return holdings; 
+  }
+  
+   public void getWatchlist(String[] args) {
+    return watchlist; 
+  }
+  
+   public void getFavorites(String[] args) {
+    return favorites; 
+  }
+  
 }
+
