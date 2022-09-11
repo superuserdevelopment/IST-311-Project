c9eaa10361f2f7547036ef73a4fe96583c28c7d5
Jangrock <113255122+Jangrock@users.noreply.github.com>
2022-09-10 22:52:13 -0400

    Final changes done.

--- src/View.java
+++ src/View.java
@@ -1,3 +1,25 @@
 
-public class View {
+public class View 
+{
+  public showMainScreen() 
+  {
+  }
+  
+  public void showAlertDialog(String title, String message) 
+  {
+  }
+  
+  public String showInputDialog(String query)
+  {
+    return String;
+  }
+  
+  public void showSplashScreen()
+  {
+  }
+  
+  public void showErrorScreen()
+  {
+  }
+  
 }
