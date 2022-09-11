2e149d0ee66974276bc805859f8bbf834ecb75f7
Jangrock <113255122+Jangrock@users.noreply.github.com>
2022-09-10 22:32:56 -0400

    Added functions

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,50 @@
 
-public class Controller {
+public class Controller 
+{
+  private Model model;
+  private View view;
+  
+  public Controller(Model model, View view) 
+  {
+		this.model = model;
+		this.view = view;
+  }
+  
+  public login();
+  {
+  }
+  
+  public newUser();
+  {
+  }
+  
+  public deleteUser();
+  {
+  }
+  
+  public deleteUser();
+  {
+  }
+  
+  public updateView();
+  { 
+  }
+  
+  public addToFavorites();
+  {
+  }
+  
+  public removeFromFavorites();
+  {
+  }
+  
+  public addToWatchlist();
+  {
+  }
+  
+  public removeFromFavorites();
+  {
+  }
+  
 
 }
