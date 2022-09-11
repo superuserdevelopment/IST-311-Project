92e7aaf436337ef67201c01726c39dd82f3d203f
Anupooja <113248548+Anupooja@users.noreply.github.com>
2022-09-10 21:53:08 -0400

    Update UserAuthenticationService.java

--- src/UserAuthenticationService.java
+++ src/UserAuthenticationService.java
@@ -1,4 +1,17 @@
 
 public class UserAuthenticationService {
+  ArrayList<String> users = new ArrayList<String>();
+  
+  public loginUser(String username, String password) {
+    
+  }
+  
+  public createNewUser(String name, String username, String password) {
+    
+  }
+  
+  public deleteUser(String username, String password) {
+    
+  }
 
 }
