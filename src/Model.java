095c1064577906acd7e7358a84c356ec5db1349c
Anupooja <113248548+Anupooja@users.noreply.github.com>
2022-09-10 22:19:25 -0400

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,44 @@
 
 public class Model {
+  
+   public login(String username, String password) {
+   
+   }
+  
+   public newUser(String name, String username, String password) {
+   
+   }
+  
+   public deleteUser(String username, String password) {
+   
+   }
+  
+   public updateView() {
+   
+   }
+  
+   public addToFavorites() {
+   
+   }
+  
+   public removeFromFavorites() {
+   
+   }
+  
+   public addToWatchlist() {
+   
+   }
+  
+   public removeFromFavorites() {
+   
+   }
+  
+   public fetchCommodity(String commodity) {
+     return commodity;
+   }
+  
+   public fetchUserDetails(String User) {
+     return user;
+   } 
 
 }
